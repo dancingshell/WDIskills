ac2d790a5eb9dc52402a04577428c91a2ea6314d
dancingshell <kaitlinjaffe@gmail.com>
2014-08-19 21:17:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WDIskills::Application.config.secret_key_base = '43632fe00733b5a3da791613e01f3a03d1d40c7944ab8c9f08b4e72a61bdbe24260611ce2bad2547fb9a08428fdaa315fbfa6ab9ec3aa3c53232dabf0f567291'
